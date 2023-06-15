f1285a31b4a34abc702588122cefe61fbf118f19
Rabbit <134244890+jp-king-1337@users.noreply.github.com>
2023-06-14 23:26:27 -0400

    Removed all comments except line 42, which I'm leaving to remind myself what line 43 does.

--- Develop/script.js
+++ Develop/script.js
@@ -32,20 +32,13 @@ $(function () {
     });
   });
 
-  // TODO: Add code to display the current date in the header of the page.
   function displayCurrentDate() {
-    // Using a modified version of ISO 8601, with T removed because I want the legibility of 8601 without the T. Bite me ISO (jk I love you ISO)
     var currentDate = dayjs().format("YYYY-MM-DD HH:mm:ss");
     $("#currentDay").text("Current Date and Time: " +currentDate);
   }
 
   displayCurrentDate();
 
-  // BOOM now the time also updates every second!!!
+  // Updates the time display every second.
   setInterval(displayCurrentDate, 1000);
 });
-
-console.log(dayjs().format("HH:mm D MMMM YYYY"));
-console.log(dayjs().format("YYYY MMMM D HH:mm"));
-console.log(dayjs().format("YYYY-MM-DD HH:mm:ss"));
-
