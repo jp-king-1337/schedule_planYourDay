dbce445c4c33869183804dde2300a433c853479e
Rabbit <134244890+jp-king-1337@users.noreply.github.com>
2023-06-14 22:35:10 -0400

    further comment removal, also removed the console.log from line 12

--- Develop/script.js
+++ Develop/script.js
@@ -1,7 +1,4 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that the code isn't run until the browser has finished rendering all the elements in the html.
 $(function () {
-  // TODO: Add a listener for click events on the save button. This code should use the id in the containing time-block as a key to save the user input in local storage. 
-  // HINT: What does `this` reference in the click listener function? How can DOM traversal be used to get the "hour-x" id of the time-block containing the button that was clicked? How might the id be useful when saving the description in local storage?
   $(".saveBtn").on("click", function btnClick() {
     var userInput = $(this).siblings(".description").val();
 
@@ -10,18 +7,12 @@ $(function () {
     localStorage.setItem(timeBlockId, userInput);
   })
 
-  // TODO: Add code to apply the past, present, or future class to each time block by comparing the id to the current hour.
-  // HINTS: How can the id attribute of each time-block be used to conditionally add or remove the past, present, and future classes? How can Day.js be used to get the current hour in 24-hour time?
-  $(document).ready(function () {
+  $(document).ready(function hourCalc() {
     var currentHour = dayjs().hour();
-    console.log(currentHour);
-    // current hour is 22 right now
 
     $(".time-block").each(function () {
-      // Get hour of time block
       var hour = parseInt($(this).attr("id").split("-")[1])
 
-      // compare to current hour
       if (hour < currentHour) {
         $(this).removeClass("present future").addClass("past");
       } else if (hour === currentHour) {
@@ -35,7 +26,7 @@ $(function () {
 
   // TODO: Add code to get any user input that was saved in localStorage and set the values of the corresponding textarea elements.
   // HINT: How can the id attribute of each time-block be used to do this?
-  $(document).ready(function () {
+  $(document).ready(function loadUserInput() {
     // starting like before - may be able to merge this with the previous TODO code
     $(".time-block").each(function () {
       // timeBlockId already declared on line 8 - not sure if problem. Think I'm just using it again but within this? Maybe I can declare it globally
